610838444a42673c7c72d256a52aa8b2ab603e32
Abby Lee <abby@ihomeeducator.com>
2020-05-15 13:13:55 -0700

    fixed dot env

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,8 @@ const { initCrypto, VirgilCrypto, VirgilAccessTokenSigner} = require('virgil-cry
 const {JwtGenerator} = require ('virgil-sdk');
 const interval = 1000;
 
-// import {APP_ID, APP_KEY_ID, APP_KEY} from '../.env'; 
-// const process = require('../.env'); 
-const process = require('../.env').config(); 
-// const appkeyid = require('../.env').APP_KEY_ID; 
-// const appkey = require('../.env').APP_KEY; 
+require('dotenv').config({path: '.env'}); 
+console.log(process.env.APP_ID);
 
 
 
@@ -23,46 +20,37 @@ async function getJwtGenerator() {
 
   // initialize JWT generator with App ID and App Key ID found in .env file. 
   //appId, apiKeyId and apiKey is already stored locally in .env in Hobbyte directory.
-console.log("App id:" +process.APP_ID);
-console.log("App key id:" +process.APP_KEY_ID);
-console.log("App id:" +process.APP_KEY);
+console.log("App id:" +process.env.APP_ID);
+console.log("App key id:" +process.env.APP_KEY_ID);
+console.log("App id:" +process.env.APP_KEY);
 console.log("hi");
-console.log("App id:" + appid);
-// console.log("App key id:" +appkeyid);
-// console.log("App id:" +appkey);
-  // return new JwtGenerator({
-  //   appId: process.env.APP_ID,
-  //   apiKeyId: process.env.APP_KEY_ID,
-  //   apiKey: virgilCrypto.importPrivateKey(process.env.APP_KEY),
-  //   // appId: keys.APP_ID,
-  //   // apiKeyId: keys.APP_KEY_ID,
-  //   // apiKey: virgilCrypto.importPrivateKey(keys.APP_KEY),
-  //   // initialize accessTokenSigner that signs users JWTs
-  //   accessTokenSigner: new VirgilAccessTokenSigner(virgilCrypto),
-  //   // JWT lifetime - 20 minutes (default)
-  //   millisecondsToLive:  20 * 60 * 1000
-  // });
+  return new JwtGenerator({
+    appId: process.env.APP_ID,
+    apiKeyId: process.env.APP_KEY_ID,
+    apiKey: virgilCrypto.importPrivateKey(process.env.APP_KEY),
+    // initialize accessTokenSigner that signs users JWTs
+    accessTokenSigner: new VirgilAccessTokenSigner(virgilCrypto),
+    // JWT lifetime - 20 minutes (default)
+    millisecondsToLive:  20 * 60 * 1000
+  });
 }
 
 const generatorPromise = getJwtGenerator();
 
 var token = {};
 
-io.on('connect', function(socket){
+io.on('connect', async (socket, res) => {
   console.log('someone connected from: ' + socket.handshake.address);
-  const generator = generatorPromise;
+  const generator = await generatorPromise;
   socket.on('pass data to server', function(info){
     
     const virgilJwtToken = generator.generateToken(info.username); // Generated Virgil Token based on username
     console.log('console log: send data to anybody', info);
-    
-    token.json({ virgilToken: virgilJwtToken.toString() });    // Convert the token into JSON object
-   
-    socket.emit ('dataFromServer', token);     //Parse the token to user.
+    console.log("token: " + virgilJwtToken.toString());
+    socket.emit ('dataFromServer', virgilJwtToken.toString());     //Parse the token to user.
     io.emit('io emit: send data to anybody', info);
   });
 });
-
 http.listen(3006, function(){
     console.log('listening on *:3006');
 });
