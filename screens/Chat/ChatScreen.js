482573ac5dc203a0ec3f217ecd11d661099f5530
viswaas prabu <viswaasprabu@viswaass-MacBook-Pro-3.local>
2020-05-29 16:13:11 -0700

    revert to working commit

--- screens/Chat/ChatScreen.js
+++ screens/Chat/ChatScreen.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { GiftedChat, Bubble } from 'react-native-gifted-chat'
 import {AsyncStorage} from "react-native";
 import {Icon} from 'react-native-elements';
-
-import nacl from 'tweet-nacl-react-native-expo'
-
 
 import API, { graphqlOperation } from '@aws-amplify/api';
 import {createMessage, deleteMessage} from '../../src/graphql/mutations';
@@ -19,24 +16,6 @@ async function deleteMessageAfterRead(messageId) {
   return await API.graphql(graphqlOperation(deleteMessage, { input: { id: messageId }}))
 }
 
-async function getPublicKey(member){
-
-  console.log("getting public key for: " + member);
-  const keyFromAWS = await API.graphql(graphqlOperation(listMessages, {filter:{to:{eq: "key"}, from: {eq: member}}})).then(
-    console.log("retrieved key")
-  ).catch(
-    (error) => {
-      console.log("Error_____________________\n" ,error)
-    }
-  );
-  const keyString = JSON.stringify(keyFromAWS.data.listMessages.items[0].payload); 
-  var key = keyString.replace(/[{"()"}]/g, '');
-  console.log("Key from AWS: <" + key + ">");
-  key = nacl.util.decodeBase64(key); 
-  console.log("key: " + key); 
-  return key;
-
-}
 /*
 Requires a payload object as defined in payload.js.
 TODO: add encrypion, add from field once schema is updated
@@ -48,89 +27,10 @@ async function sendMessage(payload) {
   var roomMembers = payload.roomMembers;
   console.log("roomMembers: ", roomMembers)
   console.log("from: "  + payload.sender);
+
+  //Encrypt payload here
+
   let payloadStr = JSON.stringify(payload);
-  console.log("payloadStr " + payloadStr); 
-  
-  var fullPayload = {
-    nonce: '',
-    key: '',
-    payloadEncrypted: '',
-    box: true,
-  };
-  
-  //Decoded string to be encrypted
-  const strDecoded = new Uint8Array(nacl.util.decodeUTF8(payloadStr))
-  console.log("strDecoded " + strDecoded); 
-
-  //new nonce being generated
-  const nonce = await nacl.randomBytes(24)
-  var encryptedNonce = nacl.util.encodeBase64(nonce);
-  
-  fullPayload.nonce = encryptedNonce; 
-  
-  //If the conversation is between 2 (box)
-  if (roomMembers.length==2){
-    console.log("using box");
-
-    for (var i = 0; i < roomMembers.length ; i++){
-      //If the member is not the sender (recipient)
-      if(roomMembers[i] != payload.sender){
-
-        //Retrieve decoded public key
-        var recipient_public_key = await getPublicKey(roomMembers[i]);
-        //Retrieve sender's private key
-        const myKeys = await AsyncStorage.getItem('keys');
-        const keysObj = JSON.parse(myKeys);
-        console.log("myKeys" + myKeys); 
-        var sender_private_key = nacl.util.decodeBase64(keysObj.secret);
-        //Create shared key
-        const mySharedKey = nacl.box.before(recipient_public_key, sender_private_key)
-        //Encrypt the message and convert to Base64 format. Base64EncryptedStr is message to be sent.
-        const EncryptedStr = nacl.box.after(strDecoded, nonce, mySharedKey)
-        const Base64EncryptedStr = nacl.util.encodeBase64(EncryptedStr)
-        fullPayload.payloadEncrypted =  Base64EncryptedStr ;
-        fullPayload = JSON.stringify(fullPayload);
-      }
-    }
-
-     
-    
-  }
-  //If the conversation is between multiple users (secretbox)
-  else{
-    console.log("using secret box");
-    fullPayload.box = false; 
-    //Generate random key
-  
-    const symmetrickey = await nacl.randomBytes(32)
-    console.log("sym key" + symmetrickey); 
-    // var symKey = nacl.util.decodeUTF8(symmetrickey);    
-
-    //encrypt decoded string with nonce and key
-    const EncryptedStr = nacl.secretbox(strDecoded, nonce, symmetrickey)
-    console.log("Encrypted STring" + EncryptedStr); 
-    //Encrypted string encoded to base 64
-    const Base64EncryptedStr = nacl.util.encodeBase64(EncryptedStr)
-    fullPayload.payloadEncrypted = Base64EncryptedStr;
-    console.log("Base64EncryptedStr " + Base64EncryptedStr); 
-
-    //Encoded key prepared to be sent
-    const key_encoded = nacl.util.encodeBase64(symmetrickey)
-    fullPayload.key = key_encoded; 
-    fullPayload = JSON.stringify(fullPayload);
-
-  }
-  
-
-  await getPublicKey(payload.roomMembers[0]); 
-  const myKeys = await AsyncStorage.getItem('keys');
-
-  console.log("Keys genererated: Public - " + myKeys);
-          const keysObj = JSON.parse(myKeys);
-
-  // console.log("Full payload:" + JSON.stringify(fullPayload)); 
-console.log("full payload" + fullPayload); 
-  console.log("private" + keysObj);
 
   for (var i = 0; i < roomMembers.length ; i++){
     if(roomMembers[i] != payload.sender){
@@ -138,7 +38,7 @@ console.log("full payload" + fullPayload);
       const package_ = {
         to: roomMembers[i],
         from: payload.sender,
-        payload: fullPayload,
+        payload: payloadStr,
       };
       console.log("package: " + JSON.stringify(package_));
       const resp = await API.graphql(graphqlOperation(createMessage, { input: package_ })).then(
@@ -153,8 +53,6 @@ console.log("full payload" + fullPayload);
   }
 }
 
-
-
 async function getAuthObject() {
   // obtains userToken and parses it to access username
   var user = await AsyncStorage.getItem("userToken");
@@ -164,8 +62,6 @@ async function getAuthObject() {
 }
 
 function createNewChatMessage(room, messages /*must be this.state*/) {
-
-
   const messageText = messages[0].text; // messages is latest message
   const date = new Date();
   const payload = new Payload(
@@ -334,6 +230,7 @@ class ChatScreen extends React.Component {
       };
   }
 
+
   componentDidMount() {
       this._isMounted = true,
 
@@ -384,7 +281,7 @@ class ChatScreen extends React.Component {
           let recpt = deleteMessageAfterRead(messageId)
           // console.log("delete: ", recpt)
           const newMessage = JSON.stringify(event.value.data, null, 2);
-          console.log("New Message on ChatScreen: " + newMessage);
+          console.log("New Message: " + newMessage);
           this.onReceive(event.value.data);
 
       }
@@ -521,82 +418,17 @@ class ChatScreen extends React.Component {
   }
 
   onReceive = async(messageObject) => {
-    
     try{
-      
-      // parse incomingMessageItem payload and save into new variable
+      //Decrypt
+      console.log("TODO: Implement decrypt");
+
+
+      /// parse incomingMessageItem payload and save into new variable
       var messageObj = messageObject.onCreateMessageByRecipient;
       var payload = messageObj.payload;
       payload = JSON.parse(payload);
-       
-         //If the roommember is the sender
-         if(payload.box){
 
-          //Acquire sender public key
-          var sender_public_key= await getPublicKey(messageObj.from);
-             
-          //Acquire recipient private key
-          const myKeys = await AsyncStorage.getItem('keys');
-          const keysObj = JSON.parse(myKeys);
-          var recipient_private_key = nacl.util.decodeBase64(keysObj.secret);
-          
-
-          //Make shared key
-          // const recipientSharedKey = nacl.box.before(recipient_private_key, sender_public_key)
-          const recipientSharedKey = nacl.box.before(sender_public_key, recipient_private_key); 
-        
-
-          //Decoded payload
-          const decoded_payload_message = nacl.util.decodeBase64(payload.payloadEncrypted)
-         
-
-          const nonceDecrypted = nacl.util.decodeBase64(payload.nonce);
-     
-          //Decrypt the payload with nonce and shared key
-          const decoded_decrypted_payload = nacl.box.open.after(decoded_payload_message, nonceDecrypted, recipientSharedKey) 
-         
-          //Encode back to UTF8. Final decrypted payload
-          const decrypted_payload = nacl.util.encodeUTF8(decoded_decrypted_payload)
-
-
-          var final_decoded_message = JSON.parse(decrypted_payload);
-          
-      
-        }else{
-          console.log("using secret box"); 
-          //Decode the provided key (in payload)
-          const keyUint8Array = nacl.util.decodeBase64(payload.key);
-  
-          //decoded message to be decrypted
-          const decoded_payload_message = nacl.util.decodeBase64(payload.payloadEncrypted);
-          const nonceDecrypted = nacl.util.decodeBase64(payload.nonce);
-          
-        
-          const decrypted = nacl.secretbox.open(decoded_payload_message, nonceDecrypted, keyUint8Array);
-        
-          if (!decrypted) {
-            throw new Error("Could not decrypt message");
-          }
-        
-          const base64DecryptedMessage = nacl.util.encodeUTF8(decrypted);
-          // return JSON.parse(base64DecryptedMessage);
-          var final_decoded_message = JSON.parse(base64DecryptedMessage);
-          
-      
-        }
-
-
-      payload = final_decoded_message;
-      /*
-      const decrypted = decrypt(payload, messageObj.key);
-      */
-      
-
-      // var nonce = payload.nonce; 
-      // var key = payload.key; 
-      // var encryptedPayload = payload.payload;
-
-      // sort message into correct room
+      //sort message into correct room
       var roomObj = await AsyncStorage.getItem(payload.roomId);
       //make room if room does not exist locally
       if(roomObj == null){
@@ -653,7 +485,7 @@ class ChatScreen extends React.Component {
         messages={this.state.messages}
         scrollToBottom
         renderBubble = {this.renderBubble}
-        loadEarlier = {this.state.loadEarlier}
+        loadEarlier = {false}
         isLoadingEarlier = {this.state.isLoadingEarlier}
         onLongPressAvatar = {user => alert(JSON.stringify(user))}
         onPressAvatar = {() => alert('short press')}
--- screens/Login/PhoneNumberVerification.js
+++ screens/Login/PhoneNumberVerification.js
@@ -1,106 +1,21 @@
 import React, { Component } from 'react';
 
-import { Button, 
-        View, 
-        Text, 
-        TouchableOpacity, 
-        StyleSheet, 
-        KeyboardAvoidingView, 
-        TextInput, 
-        Image, 
-        Keyboard, 
-        ScrollView, 
-        AsyncStorage } from 'react-native';
+import { View,
+         Text, 
+         TouchableOpacity, 
+         KeyboardAvoidingView, 
+         TextInput, 
+         Image, 
+         ScrollView, 
+         AsyncStorage,
+         Alert } from 'react-native';
 import Amplify, { Auth } from 'aws-amplify';
 import {styles} from '../../styles/styles'
 import Icon from 'react-native-vector-icons/Ionicons'
-import nacl from 'tweet-nacl-react-native-expo'
-
-import API, { graphqlOperation } from '@aws-amplify/api';
-import {ActionType, Payload} from '../../src/payload';
-import {listMessages} from '../../src/graphql/queries';
-import {createMessage, updateMessage} from '../../src/graphql/mutations';
-import awsconfig from '../../aws-exports';
-API.configure(awsconfig);
 
 /*=====================================================*/
 /*            Phone Verification Screen                */
 /*=====================================================*/
-
-
-async function generateKeys(user) {
-
-  // key = "xxxx dummy keyxxxx"; 
-
-  // create and store keys in local storage 
-  const keyPair = await nacl.box.keyPair() 
-  const {publicKey, secretKey} = keyPair 
-  var pKey = nacl.util.encodeBase64(publicKey); 
-  var sKey = nacl.util.encodeBase64(secretKey); 
-
-
-  const keys = {
-    public: pKey, 
-    secret: sKey, 
-  };
-  await AsyncStorage.setItem('keys',JSON.stringify(keys));
-  // store keys in local storage
- 
-
-  // check if key exists on AWS 
-  //console.log("user checking for key: " + user.username);
-  const keyFromAWS = await API.graphql(graphqlOperation(listMessages, {filter:{to:{eq: "key"}, from: {eq:user.username}}})).then(
-    console.log("key from AWS object: " + JSON.stringify(keyFromAWS))
-  ).catch(
-    (error) => {
-      console.log("Error_____________________\n" ,error)
-    }
-  );
-
-  if (keyFromAWS.data.listMessages.items.length == 0){ // new key
-    const package_ = {
-      to: "key", 
-      from: user.username, 
-      payload: pKey,
-    };
-  
-    const resp = await API.graphql(graphqlOperation(createMessage, { input: package_ })).then(
-      console.log("AWS Success stored key on AWS")
-    ).catch(
-      (error) => {
-        console.log("Error_____________________\n" ,error)
-      }
-    );
-
-  } else { // Key exists... update! 
-    console.log("update key"); 
-    const messageID = keyFromAWS.data.listMessages.items[0].id;
-    console.log("message ID " + messageID); 
-    console.log("now updating key"); 
-    const updateObj = {
-      input: {
-        id: messageID, 
-        payload: pKey}, 
-      condition: {to:{eq: "key"}, from:{eq:user.username}}
-
-    };
-    const updateKey = await API.graphql(graphqlOperation(updateMessage, updateObj)).then(
-      console.log("update key message: " + JSON.stringify(updateKey))
-    ).catch(
-      (error) => {
-        console.log("Error_____________________\n" ,error)
-      }
-    );
- 
-
-  }
- 
-  // if key exists, update 
-    // else store as a new message 
-  // delete key and store new one
-  
-
-}
 export default class PhoneNumberVerification extends React.Component {
 
 
@@ -134,7 +49,6 @@ export default class PhoneNumberVerification extends React.Component {
             keyboardType="phone-pad"
             autoFocus={true}
             onSubmitEditing = {this._loginAsync}
-            //onSubmitEditing = {generateKeys(this.state.user)}
             autoCapitalize='none'
             autoCorrect={false}
             value={this.state.verificationCode}
@@ -153,7 +67,15 @@ export default class PhoneNumberVerification extends React.Component {
 
         <TouchableOpacity style={styles.resetContainer}>
                 <Text style={styles.texts}
-                  onPress={this._resetAsync}>Changed phone number?</Text>
+                  onPress={
+                    ()=>{
+                      this.setState({verificationCode:''})
+                      this.props.navigation.navigate("SignIn")
+                    }
+                  }>
+                    Verification code not working?
+                    Click to return to the Login page and try to login again!
+                </Text>
         </TouchableOpacity>
 
           </ScrollView>
@@ -166,11 +88,46 @@ export default class PhoneNumberVerification extends React.Component {
 
   /*--------------------Async------------------------*/
     getPromptMessage(){
-      if(this.props.navigation.getParam('authType', 'none') == 'verify_email') {
-        return "Enter Email Verification Code"
+      let authType = this.props.navigation.getParam('authType', 'none')
+      if(authType == 'verify_email') {
+        return "Enter Email Verification Code To Verify Your Email"
+      } else if (authType == 'signin'){
+        return "Enter Text Verification Code To Login"
+      } else if(authType == 'signup'){
+        return "Enter Text Verification Code To Verify Your Phone Number"
       } else {
-        return "Enter Text Verification Code"
+        return "Enter Unknown Code For Unkown Reason"
       }
+    }
+    handelAWSError(authType, error) {
+      let errorType;
+      if(authType == 'signup') {
+        errorType = 'New User Confirmation'
+      } else if(authType == 'signin') {
+         errorType = "Login"
+      } else if(authType == 'verify_email') {
+        errorType = 'Email Verification'
+      } else {
+        errorType = error.code
+      }
+      
+      let errorMessage;
+      console.log("This is error code: ", error.code)
+      if(error.code =='NotAuthorizedException') {
+        return
+      } else if(error.code == 'CodeMismatchException') {
+        errorMessage = "Invalid Verification Code Entered!"
+      } else if(error.code == 'LimitExceededException') {
+        errorMessage = 'You Have Exceeded The Verification Code Limit. Please Wait A While And Then Try Again.'
+      } //else if (error.message == "Invalid session for the user, session is expired."){
+      //   errorMessage = "Your Verification Code Has Expired. Please Resend The Code."
+      // } 
+      else {
+        errorMessage = error.message
+      }
+      Alert.alert(errorType + " Error!", errorMessage)
+      return
+
     }
     _loginAsync = async () => {
       // TODO - fetch user token and verify user identity
@@ -181,24 +138,42 @@ export default class PhoneNumberVerification extends React.Component {
       console.log("Varification type: ", this.state.authType)
       console.log("this is AuthType: ", this.props.navigation.getParam('authType'))
 
-      if(this.props.navigation.getParam('authType', 'none') == 'signup') {
+      let authType = this.props.navigation.getParam('authType', 'none');
+      if(this.state.verificationCode == '') {
+        
+        let errorType;
+        if(authType == 'signup') {
+          errorType = 'New User Confirmation'
+        } else if(authType == 'signin') {
+           errorType = "Login"
+        } else if(authType == 'verify_email') {
+          errorType = 'Email Verification'
+        } else {
+          errorType = 'Unknown Type'
+        }
+        Alert.alert(errorType + " Error!",'The Verification Code Field Can Not Be Empty!')
+        return
+      } 
+
+      if(authType == 'signup') {
         console.log("------------This is signup-----------------")
         Auth.confirmSignUp(this.state.username, this.state.verificationCode)
         .then(
             ()=>{
               console.log('successful confirm sign up!')
-              AsyncStorage.setItem("userToken", JSON.stringify(Auth))
+              //AsyncStorage.setItem("userToken", JSON.stringify(Auth))
               //Need to sign in so that the user is authenticated
+              this.setState({verificationCode:''})
               this.props.navigation.navigate("SignIn");
             }
           )
         .catch(
           (err) => {
             console.log('error confirming signing up!: ', err);
-                alert('error confirming signing up!: '+ err.message);
+            this.handelAWSError(authType, err)
           }
         )
-      } else if(this.props.navigation.getParam('authType', 'none') == 'signin') {
+      } else if(authType == 'signin') {
         console.log("------------This is signin-----------------")
         // remove from final version
         // if (this.state.verificationCode==1111){
@@ -212,7 +187,7 @@ export default class PhoneNumberVerification extends React.Component {
         .then(
           () => {
             console.log('successful confirm sign in!');
-            AsyncStorage.setItem("userToken",JSON.stringify(Auth))
+            //AsyncStorage.setItem("userToken",JSON.stringify(Auth))
 
             Auth.currentAuthenticatedUser()
               //Check if email is verified, if not verify it, else go to main
@@ -232,13 +207,13 @@ export default class PhoneNumberVerification extends React.Component {
                       )
                       .catch(
                         (err)=>{
-                          console.log("Error in init email verification: ", err)
+                          this.handelAWSError(authType, err)
                         }
                       )
 
                   } else {
-                    generateKeys(this.state.user); 
-                    //alert("would navigate to home but just for testing");
+                    AsyncStorage.setItem("userToken",JSON.stringify(Auth))
+                    this.setState({verificationCode:''})
                     this.props.navigation.navigate('Home' );
                   }
                 })
@@ -249,10 +224,14 @@ export default class PhoneNumberVerification extends React.Component {
               )
           }
         )
-        .catch(err => {console.log('error confirming signing in!: ', err);
-                alert('error confirming signing in!: '+err.message);});
+        .catch(
+          err => {
+                console.log('error confirming signing in!: ', err);
+                this.handelAWSError(authType, err)
+              }
+        );
 
-      } else if(this.props.navigation.getParam('authType', 'none') == 'verify_email') {
+      } else if(authType == 'verify_email') {
         console.log("------------Verifying the email-----------------")
         console.log("comming from: ", this.props.navigation.getParam('authType', 'none'))
 
@@ -260,19 +239,45 @@ export default class PhoneNumberVerification extends React.Component {
             ()=>{
               console.log("email has been verified.")
               //We came form signup so we want to go to signin...
-              this.props.navigation.navigate("SignIn");
+              AsyncStorage.setItem("userToken",JSON.stringify(Auth))
+              this.setState({verificationCode:''})
+              this.props.navigation.navigate("Home");
 
             }
         ).catch((err)=>{
-            console.log("Error verifing Email, comming form: ", err)
+          this.handelAWSError(authType, err)
         })
       }
     };
 
-    _resetAsync = async () => {
-      // TODO - fetch user token and verify user identity
-      // await AsyncStorage.setItem('userToken', 'abc'); // comment back in when storage set up
-      console.log("Redirecting to phone reset page");
-      this.props.navigation.navigate('PhoneReset');
-    };
 }
+
+// error name:  - happens when you press submite twicec DONE
+// when code field is empty, app chashes. add check to see that it is not empty. 
+// White spage is not an empty string. DONE
+// CodeMismatchException - when the code is incorrect. DONE
+
+//Clear username / password upon submit loginy
+//Want to add resend code button.
+
+/*
+Happend when you wait too longe before submittiong the code.
+error confirming signing in!:  Object {
+  "code": "NotAuthorizedException",
+  "message": "Invalid session for the user, session is expired.",
+  "name": "NotAuthorizedException",
+}
+*/ 
+
+
+//Too many email reset codes sent code: LimitExceededException DONE
+
+
+/*
+When you enter the same code twice.
+error confirming signing in!:  Object {
+  "code": "NotAuthorizedException",
+  "message": "Invalid session for the user, session can only be used once.",
+  "name": "NotAuthorizedException",
+}
+*/
